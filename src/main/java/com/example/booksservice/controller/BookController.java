d4ddb6be69776c04f064d5357f90a695a57abf59
Arina <arina.milyutina.0005@gmail.com>
2025-01-06 23:08:34 +0300

    added method for create and update book with feign client

--- src/main/java/com/example/booksservice/controller/BookController.java
+++ src/main/java/com/example/booksservice/controller/BookController.java
@@ -1,7 +1,9 @@
 package com.example.booksservice.controller;
 
+import com.example.booksservice.dto.BookDto;
 import com.example.booksservice.entity.Book;
 import com.example.booksservice.exception.BookNotFoundException;
+import com.example.booksservice.feignclient.UserClient;
 import com.example.booksservice.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -18,7 +21,16 @@ import java.util.Optional;
 public class BookController {
     @Autowired
     private BookService bookService;
+    @Autowired
+    private UserClient userClient;
 
+    @PreAuthorize(value = "hasRole('ADMIN')")
+    @PostMapping("/create")
+    public ResponseEntity<Book> createBook(@Valid @RequestBody BookDto bookDto) {
+        Long currentUserId = userClient.getCurrentUserId();
+        bookDto.setUserId(currentUserId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(bookService.createBook(bookDto));
+    }
 
     @GetMapping("/books")
     public ResponseEntity<List<Book>> findAll() throws BookNotFoundException {
@@ -47,7 +59,16 @@ public class BookController {
         return new ResponseEntity<>(bookService.deleteBookByISBN(ISBN), HttpStatus.OK);
     }
 
-}
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/admin/update/{ISBN}")
+    public ResponseEntity<Book> updateByISBN(@PathVariable String ISBN, @Valid @RequestBody BookDto bookDto) throws BookNotFoundException {
+        Long currentUserId = userClient.getCurrentUserId();
+        bookDto.setUserId(currentUserId);
+        Book book = bookService.updateBookByISBN(ISBN, bookDto);
+        return ResponseEntity.ok(book);
 
+    }
+
+}
 
 
