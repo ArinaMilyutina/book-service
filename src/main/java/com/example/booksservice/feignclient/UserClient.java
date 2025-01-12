8d9fa5cb54a24e0062741e0651afbe5cfd74ce04
Arina <arina.milyutina.0005@gmail.com>
2025-01-13 00:05:58 +0300

    renamed url /user/current-user -> external/user/current-user

--- src/main/java/com/example/booksservice/feignclient/UserClient.java
+++ src/main/java/com/example/booksservice/feignclient/UserClient.java
@@ -3,9 +3,9 @@ package com.example.booksservice.feignclient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @FeignClient(name = "library2")
 public interface UserClient {
-
-    @GetMapping("/user/current-user")
+    @GetMapping("/external/user/current-user")
     Long getCurrentUserId();
 }
