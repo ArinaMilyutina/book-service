ae3bcd881a58e03afc9c3f0b52eed473393397f2
Arina <arina.milyutina.0005@gmail.com>
2025-01-13 00:04:00 +0300

    deleted logger

--- src/main/java/com/example/booksservice/configuration/FeignRequestInterceptor.java
+++ src/main/java/com/example/booksservice/configuration/FeignRequestInterceptor.java
@@ -2,8 +2,6 @@ package com.example.booksservice.configuration;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -12,16 +10,11 @@ import org.springframework.stereotype.Component;
 public class FeignRequestInterceptor implements RequestInterceptor {
     private static final String AUTHORIZATION = "Authorization";
     private static final String BEARER = "Bearer ";
-    private static final Logger logger = LoggerFactory.getLogger(FeignRequestInterceptor.class);
 
     @Override
     public void apply(RequestTemplate template) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.getCredentials() != null) {
-            String token = authentication.getCredentials().toString();
-            template.header(AUTHORIZATION, BEARER + token);
-        } else {
-            logger.warn("No authentication found or credentials are null");
-        }
+        String token = authentication.getCredentials().toString();
+        template.header(AUTHORIZATION, BEARER + token);
     }
 }
